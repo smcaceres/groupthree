4710654714ca87508e419c167339f5681a6df576
Kyrsten <ksurbayan@gmail.com>
2021-11-16 19:27:09 -0800

    home and dashboard route basics created, waiting on models

--- controllers/api/vote-routes.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-const { User, Post, Comment, Vote } = require('../../models');
-
-module.exports = router;
---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,91 @@
+const router = require('express').Router();
+// const sequelize = require('../config/connection');
+// const { User, Post, Comment } = require('../models');
+
+// GET /dashboard (display all logged in user's posts, waiting on authentication)
+router.get('/', (req, res) => {
+    Post.findAll({
+        where: {
+            // id from the session
+            id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'rating',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            // serialize data for template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('dashboard', { posts, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// GET /dashboard/edit/:id (edit a post)
+router.get('/edit/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'rating',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if(!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id!' });
+                return;
+            }
+
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
