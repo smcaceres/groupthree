4e252e8fe29e05da156cb9382fef2a240472d60f
kg-phantom <ksurbayan@gmail.com>
2021-11-28 23:34:28 -0800

    remove gitignore files

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-async function editFormHandler(event) {
-    event.preventDefault();
-
-    const title = document.querySelector('input[name="post-title"]').value.trim();
-    const content = document.querySelector('input[name="content"]').value.trim();
-    console.log(title);
-    console.log(content);
-
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-      
-      const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          post_id: id,
-          title,
-          content
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-
-}
-
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
-=======
 async function editPostHandler(event) {
     event.preventDefault();
 
@@ -89,4 +54,3 @@ async function deletePostHandler(event) {
 document.querySelector('.edit-post-form').addEventListener('submit', editPostHandler);
 
 document.querySelector('#delete-btn').addEventListener('click', deletePostHandler);
->>>>>>> sc-style
