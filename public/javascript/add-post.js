08eed7470df819542b3aa7927690e292ea4999db
kg-phantom <ksurbayan@gmail.com>
2021-11-29 00:23:18 -0800

    ficx conflicts

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,30 +1,6 @@
 async function newFormHandler(event) {
     event.preventDefault();
   
-<<<<<<< HEAD
-    const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('input[name="content"]').value;
-  
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        content
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
-  };
-  
-document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
-=======
     const title = document.querySelector('input[name="movie-title"]').value;
     const rating = document.querySelector('select[name="rating"]').value;
     const content = document.querySelector('textarea[name="content"]').value;
@@ -54,4 +30,3 @@ document.querySelector('#new-post-form').addEventListener('submit', newFormHandl
 }
   
 document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
->>>>>>> 0430541e4d7261b47e2a80f061274f62d6a0275f
